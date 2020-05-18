86cc918e8d73f554a3880789bb63c363dd130417
Wendel Silva <wfrsilva@gmail.com>
2020-05-18 01:32:26 -0300

    42/45 Adicionando Pontuação #03
    42/45 Adicionando Pontuação #03
    https://www.cod3r.com.br/courses/take/aprenda-flutter-dart-e-construa-apps-ios-e-android/lessons/10518569-adicionando-pontuacao-03

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
               perguntaSelecionada: _perguntaSelecionada,
               quandoResponder: _responder,
             )
-          : Resultado(),
+          : Resultado(_pontuacaoTotal),
     ));
   } //build
 
--- lib/resultado.dart
+++ lib/resultado.dart
@@ -2,13 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class Resultado extends StatelessWidget {
+  
+  final int pontuacao;
+
+  Resultado(this.pontuacao);
+
+  String get fraseResultado{
+    if(pontuacao <8){
+      return 'Parabéns!\n\n${pontuacao}';
+    }else if(pontuacao < 12){
+      return 'Você é bom!\n\n${pontuacao}';
+    }else if(pontuacao < 16){
+      return 'Impressionante!\n\n${pontuacao}';
+    }else{
+      return 'Nivel Jedi!\n\n${pontuacao}';
+    }
+  }//fraseResultado
+
   @override
   Widget build(BuildContext context) {
     return Center(child: Text(
-      'Parabéns !!!', 
+      fraseResultado, 
+      textAlign: TextAlign.center,
       style: TextStyle(
         fontSize: 28, 
-        color: Colors.blue[900]
+        color: Colors.blue[900],
+        
         ),
       
       )
